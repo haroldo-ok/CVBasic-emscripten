ee862f8bd362772e63ec46906bd0be7916abec91
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2025-05-24 09:40:30 -0300

    Add a custom shell.

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 setlocal
 cd CVBasic
-emcc cvbasic.c node.c driver.c cpu6502.c cpuz80.c cpu9900.c -o ../build/cvbasic.html 
+emcc --shell-file ../src/shell_minimal.html cvbasic.c node.c driver.c cpu6502.c cpuz80.c cpu9900.c -o ../build/cvbasic.html 
 cd ..
 endlocal
