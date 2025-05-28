2cdaafe86c9b82abe38cb1333726cc13c9fb523b
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2025-05-28 06:23:00 -0300

    Try to embed asm files.

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 setlocal
 cd CVBasic
-emcc -s INVOKE_RUN=0 -s NO_EXIT_RUNTIME=1 --shell-file ../src/shell_minimal.html cvbasic.c node.c driver.c cpu6502.c cpuz80.c cpu9900.c -o ../build/cvbasic.html 
+emcc -s INVOKE_RUN=0 -s NO_EXIT_RUNTIME=1 --embed-file *.asm --shell-file ../src/shell_minimal.html cvbasic.c node.c driver.c cpu6502.c cpuz80.c cpu9900.c -o ../build/cvbasic.html 
 cd ..
 endlocal
