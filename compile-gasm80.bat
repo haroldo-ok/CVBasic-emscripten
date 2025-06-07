9e18a9411bdc01c1cd9769c50054e6b877b44b49
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2025-06-07 08:13:44 -0300

    Try to rename `Module` variable for `gasm80`
    Using version `0.27.0` (last available "portable" version), `MODULARIZE`
    isn't recognized and a hacky way to reload the var does not work,
    either.

--- compile-gasm80.bat
+++ compile-gasm80.bat
@@ -1,6 +1,6 @@
 copy src\FileSaver.js build
 setlocal
 cd gasm80
-emcc -s INVOKE_RUN=0 -s NO_EXIT_RUNTIME=1 -s MODULARIZE=1 -s EXPORT_NAME="'Gasm80'" --embed-file ../src/embed_gasm80/@/ --shell-file ../src/shell_minimal_gasm80.html gasm80.c -o ../build/gasm80.html 
+emcc -s INVOKE_RUN=0 -s NO_EXIT_RUNTIME=1 -s EXPORT_NAME="'Gasm80'" --embed-file ../src/embed_gasm80/@/ --shell-file ../src/shell_minimal_gasm80.html gasm80.c -o ../build/gasm80.html 
 cd ..
 endlocal
